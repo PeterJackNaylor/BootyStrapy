d0a3cd053414192c34787bd678cb428731cc47ef
Peter Naylor <peter.naylor@ensae.fr>
2015-05-06 12:22:09 +0200

    adding data

--- test.R
+++ test.R
@@ -14,7 +14,8 @@ X=rnorm(n,mu,sigma)
 curve(dnorm(x,mu,sigma),from=mu-2*sigma^2,to=mu+2*sigma^2)
 
 ##Noyau
-K=function(x,X,h){return(dnorm((x-X)/h))}
+K=function(x,h){
+  return(dnorm((x-X)/h))}
 
 
 ##AMISE et Sheather Jones
@@ -29,11 +30,21 @@ R_f_2_prime=3/(8*sqrt(pi)*sigma^5)
 sigma_K=1
 n
 
-h_star.s= (R_K/(n.s*R_f_2_prime*sigma_K^4))^1/5
+h_star.s = (R_K/(n.s*R_f_2_prime*sigma_K^4))^1/5
 
 #With bootstrap
-
-X_10=sample(X,B.s[1])
+bootstrap=function(B,x){
+  ans=c()
+  n=length(x)
+  for (i in 1:B){
+    
+    ans=cbind(ans,sample(x,n,replace=T))
+    
+  }
+  
+  return(ans)
+}
+X_10=bootstrap(B,X)
 X_50=sample(X,B.s[2])
 X_100=sample(X,B.s[3])
 X_250=sample(X,B.s[4])
@@ -60,40 +71,60 @@ L_6=function(x){
   
 }
 
-
-X_val=X
-n=length(X_val)
-lambda.hat=IQR(X_val)
-a=0.920*lambda.hat*n^(-1/7)
-b=0.912*lambda.hat*n^(-1/9)
-
-
-diff_table=t(outer(X_val,X_val, `-`)) 
-T_b=-sum(L_6(diff_table/a))/(n*(n-1)*b^7)
-S=function(a){return(sum(L_4(diff_table/a))/(n*(n-1)*a^5))}
-S_a=S(a)
-alpha_func=function(h){return(1.357*(S_a/T_b)^(1/7)*h^(5/7))}
-
-equation=function(h){
-  value=R_K/(sigma_K*S(alpha_func(h)))
+find_h=function(matrice_B){
+  h.s=rep(0,B)
+  for(i in 1:B){
+  X_val=matrice_B[,i]
+  n=length(X_val)
   
-  return((value/n)^(1/5)-h)
-}
+  lambda.hat=IQR(X_val)
+  a=0.920*lambda.hat*n^(-1/7)
+  b=0.912*lambda.hat*n^(-1/9)
 
-equation(0.9)
-ans=rep(0,1000)
-h=rep(0,1000)
-for (i in 1:1000){
-  if (i==1){
-    h[i]=0.001
-  }else{
-    h[i]=0.001+h[i-1]
+
+  diff_table=t(outer(X_val,X_val, `-`)) 
+  
+  T_b=-sum(L_6(diff_table/a))/(n*(n-1)*b^7)
+  
+  S=function(a){return(sum(L_4(diff_table/a))/(n*(n-1)*a^5))}
+  
+  S_a=S(a)
+  
+  alpha_func=function(h){return(1.357*(S_a/T_b)^(1/7)*h^(5/7))}
+
+  equation=function(h){
+  
+    value=1/(2*n*sqrt(pi)*S(alpha_func(h)))
+    
+    return((value)^(1/5)-h)
   }
-  ans[i]=equation(h[i])
   
-} 
-plot(h,ans)
-h_opti_hat <- uniroot(equation,c(0.001,1))$root
+  h.s[i]=uniroot(equation,c(0.00001,1))$root
+
+  }
+
+  return(h.s)
+
+}
+h.s=find_h(X_10)
+h.s
+h_star.s
+
+h=mean(h.s)
+h=h_star.s[1]
+
+### boot strap avec les vraies fonctions
 
 
-###
+plot(density(X,bw="SJ-dpi"),col="blue")
+
+matrice_B=bootstrap(B,X)
+  
+res=rep(0,B)
+for(i in 1:B){
+  X_val=matrice_B[,i]
+  h_b=density(X,bw="SJ-dpi")$bw
+  res[i]=h_b
+}
+mean(res)
+density
